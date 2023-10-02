3098017e407597c5cb97fbfca63c4afd7b3309ee
Subsaw <subsaw@leprogrammers.com>
2023-10-02 18:21:38 +0200

    Initial

---
+++ Calculator/Calculator/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Calculator.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Calculator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Imie = "Adda";
+            ViewBag.godzina = DateTime.Now.Hour;
+            ViewBag.powitanie = ViewBag.godzina < 17 ? "Dzien dobry" : "Dobry wieczór";
+
+
+            Dane[] osoby =
+            {
+                new Dane {Name = "Anna", Surname="Kowalska"},
+                new Dane {Name = "Jan", Surname="Zbedny"},
+                new Dane {Name = "Mateusz", Surname="Uwalowisz"},
+            };
+
+            return View(osoby);
+
+
+        }
+
+        public IActionResult Urodziny( Urodziny urodziny)
+        {
+            ViewBag.powitanie = $"Witaj {urodziny.Imie} masz {DateTime.Now.Year - urodziny.Rok} lat";
+            return View();
+        }
+
+        public IActionResult Kalkulator(Kalkulator kalkulator)
+        {
+            ViewBag.wynik = 0;
+            if(kalkulator.znak == "-")
+            {
+                ViewBag.wynik = kalkulator.cyfra1 - kalkulator.cyfra2;
+            }else if(kalkulator.znak == "+")
+            {
+                ViewBag.wynik = kalkulator.cyfra1 + kalkulator.cyfra2;
+            }
+            else if (kalkulator.znak == "/")
+            {
+                ViewBag.wynik = kalkulator.cyfra1 / kalkulator.cyfra2;
+            }
+            else if (kalkulator.znak == "*")
+            {
+                ViewBag.wynik = kalkulator.cyfra1 * kalkulator.cyfra2;
+            }
+
+            ViewBag.wynik = $"{kalkulator.cyfra1} {kalkulator.znak} {kalkulator.cyfra2} = {ViewBag.wynik}"; 
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Calculator/Calculator/Models/Kalkulator.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator.Models
+{
+    public class Kalkulator
+    {
+        public int cyfra1 { get; set; }
+        public int cyfra2 { get; set; }
+        public string znak { get; set; }
+    }
+}
